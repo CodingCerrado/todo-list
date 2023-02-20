19f01fd021075b3e8417bade508c2c765b3f6611
Alan Mortoni <mortoni.alan@hotmail.com>
2023-02-20 19:10:38 +1000

    fix: layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
-import React from 'react';
-import logo from './logo-coding-cerrado.png';
-import './App.scss';
+import "./App.scss";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} alt="logo" />
-        <p>
-          Coding Cerrado
-        </p>
-        <a
-          className="App-link"
-          href="https://github.com/CodingCerrado"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Coding Cerrado  - Github Page
-        </a>
-      </header>
+    <div className="app">
+      <div>Header</div>
+      <div>Content</div>
     </div>
   );
 }
