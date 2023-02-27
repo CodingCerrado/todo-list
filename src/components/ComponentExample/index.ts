3153d86c2a40c5a0c72c37020d231bdf157867ba
Alan Mortoni <mortoni.alan@hotmail.com>
2023-02-27 15:11:35 +1000

    feat: component example

---
+++ src/components/ComponentExample/index.ts
@@ -0,0 +1,23 @@
+export { default } from "./ComponentExample";
+
+// NOTE: this file name is `index.ts` and not `index.tsx` because it is not a React component but a file that exports a React component
+
+/*
+The code export {default} from './ComponentExample'; is a syntax for exporting the default export of a module in JavaScript.
+
+When a module has a default export, it can be imported with a simplified syntax like import MyComponent from './MyComponent' instead of using the longer syntax like import { default as MyComponent } from './MyComponent'.
+
+The export {default} from './Component'; syntax allows you to export the default export of a module directly from another module. This can be useful if you want to re-export the default export of one module as the default export of another module.
+
+For example, suppose you have a module Component.js that exports a React component as its default export, and you want to use this component in another module App.js and export it as the default export of App.js. In this case, you can use the export {default} from './Component'; syntax in App.js like so:
+
+This allows you to import the component from App.js with the simplified syntax import MyComponent from './App'.
+
+*/
+
+/**
+ * Simplifies imports: By having an index file in the component folder, you can import the component using the folder path instead of the file path. For example, instead of writing import Button from './Button/Button', you can write import Button from './Button'. This can make the code easier to read and reduce errors when refactoring.
+ * Organizes related files: If you have multiple files associated with a component (such as styles or tests), having an index file can provide a central place to organize them. For example, you can have a Button.js file for the component logic, a Button.styles.js file for the styles, and a Button.test.js file for the tests, all within the Button folder.
+ * Encapsulates implementation details: By using an index file to export the component, you can hide the implementation details of the component from other parts of the application. This can help with code maintainability and reduce the risk of breaking changes when modifying the component's internals.
+ * Overall, using an index file inside a component folder can provide benefits in terms of code organization, maintainability, and readability.
+ */
