90c9cdaa0dd04d78feb66176f10aeb3551d6fd39
Alan Mortoni <mortoni.alan@hotmail.com>
2023-02-27 22:17:45 +1000

    fix: comment

--- src/components/ComponentExample/index.ts
+++ src/components/ComponentExample/index.ts
@@ -1,6 +1,6 @@
 export { default } from "./ComponentExample";
 
-// NOTE: this file name is `index.ts` and not `index.tsx` because it is not a React component but a file that exports a React component
+// NOTE: this file name is `index.ts` and not `index.tsx` because it is not a React component but an entry file that exports a React component
 
 /*
 The code export {default} from './ComponentExample'; is a syntax for exporting the default export of a module in JavaScript.
@@ -16,6 +16,7 @@ This allows you to import the component from App.js with the simplified syntax i
 */
 
 /**
+ * Why should we have index.ts files in component folders?
  * Simplifies imports: By having an index file in the component folder, you can import the component using the folder path instead of the file path. For example, instead of writing import Button from './Button/Button', you can write import Button from './Button'. This can make the code easier to read and reduce errors when refactoring.
  * Organizes related files: If you have multiple files associated with a component (such as styles or tests), having an index file can provide a central place to organize them. For example, you can have a Button.js file for the component logic, a Button.styles.js file for the styles, and a Button.test.js file for the tests, all within the Button folder.
  * Encapsulates implementation details: By using an index file to export the component, you can hide the implementation details of the component from other parts of the application. This can help with code maintainability and reduce the risk of breaking changes when modifying the component's internals.
