efad0d517e8a5418809a145b70d88bdea8383c20
Jefferson Silva <111249250+Jeffersonlpu@users.noreply.github.com>
2023-09-06 11:55:17 -0300

    Atualizando o todo.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,29 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import "./App.scss";
 import CreateToDo from "./components/CreateToDo";
 import Header from "./components/Header";
 import Todo from "./components/Todo";
 
 const App = () => {
-  const [items, setItems] = useState<(string | null)[]>([
-    "Item 1",
-    "Item 2",
-    "Item 3",
-  ]);
-
-  const [hasNull, setHasNull] = useState(false);
+  const [items, setItems] = useState<string[]>(["Item 1", "Item 2", "Item 3"]);
 
   const handleCreateToDo = () => {
-    setItems([...items, null]);
+    setItems([...items, ""]);
   };
 
-  useEffect(() => {
-    setHasNull(items.includes(null));
-  }, [items]);
+  const handleUpdateTodo = (id: string, newText: string) => {
+    const updatedItems = items.map((item) => {
+      if (item === id) {
+        return newText;
+      } else {
+        return item;
+      }
+    });
+    setItems(updatedItems);
+  };
 
-  const handleRemoveTodo = (id: string | null) => {
-    setItems(items.filter((item) => item !== id && item !== null));
+  const handleRemoveTodo = (id: string) => {
+    setItems(items.filter((item) => item !== id));
   };
 
   return (
@@ -30,17 +31,22 @@ const App = () => {
       <Header />
       <div>Content</div>
       <div>
-        {items.map((item: string | null, index) => (
+        {items.map((item: string, index) => (
           <Todo
             key={item}
             id={item}
             todoId={index.toString()}
             onDelete={() => handleRemoveTodo(item)}
+            onUpdate={(newText: string) => handleUpdateTodo(item, newText)}
+            itemText={item}
           />
         ))}
 
         <div>
-          <CreateToDo onAdd={handleCreateToDo} hasNull={hasNull} />
+          <CreateToDo
+            onAdd={handleCreateToDo}
+            hasEmptyString={items.includes("")}
+          />
         </div>
       </div>
     </div>
